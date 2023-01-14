446d86e05bb5f0ba8fa0b720f8085c7ed380f568
StefanPacola <stevo.pacola@gmail.com>
2023-01-14 21:48:44 +0100

    created a form in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 const App = () => {
-	return <div>App</div>;
+	const submitHandler = () => {};
+
+	return (
+		<div className="App">
+			<h1>Welcome to Translator App</h1>
+			<h2>by Stephanus</h2>
+			<p>Scroll down to see how the app works.</p>
+			<form onSubmit={submitHandler}>
+				<fieldset>
+					<legend>
+						<label htmlFor="text">Search for a word:</label>
+					</legend>
+					<input
+						id="text"
+						type="text"
+					/>
+					<button type="submit">Search</button>
+				</fieldset>
+			</form>
+		</div>
+	);
 };
 
 export default App;
