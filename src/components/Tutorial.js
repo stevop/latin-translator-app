a9e1629f24c022597e6c7edc19d686b0415788ff
StefanPacola <stevo.pacola@gmail.com>
2023-01-17 18:14:03 +0100

    changed text in tutorial component

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -10,9 +10,7 @@ const Tutorial = () => {
 				Hello and thank you for using my web application. It is <b>free</b> to
 				use and I take <b>no responsibility</b> for what is being displayed as a
 				result of searching, for I am not the creator of API on which the search
-				results are dependend on. In this moment, the application is only
-				accessible through the web, but I plan to create a native versions for
-				Android and iOS if there will be a demand. Thanks again and enjoy.
+				results are dependend on. Thank you again and enjoy!
 			</p>
 		</div>
 	);
